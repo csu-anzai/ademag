5d34ddbb578b51abad0ba22419dc7d2b7e6ea8a9
andresvcc <43545168+andresvcc@users.noreply.github.com>
2019-07-24 00:33:21 +0200

    configurando cosas para comenzar a trabajar

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.urlencoded({extended: true}))
   }
 
   app.get('/',(req,res)=>{
-    res.send('Projet 2019 Test')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   })
 
   // get test
