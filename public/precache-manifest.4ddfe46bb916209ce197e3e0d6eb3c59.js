408e6b5bb356cefaa47464c93863b19e2a03aa1e
andresvcc <43545168+andresvcc@users.noreply.github.com>
2019-07-24 09:52:12 +0200

    add react app (esto es un test)

---
+++ public/precache-manifest.4ddfe46bb916209ce197e3e0d6eb3c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c31b0acd50c2ac03e5b26f7c3709fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb4c92864d5e13831b9",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7fb4c92864d5e13831b9",
+    "url": "/static/js/main.e472dcfb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
