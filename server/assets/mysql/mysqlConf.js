dcef4e6abd35462493789a57795bb04f6a3eab83
andresvcc <andres.caballero@etu.unige.ch>
2019-08-21 11:38:13 +0200

    effacer les acces au server

--- server/assets/mysql/mysqlConf.js
+++ server/assets/mysql/mysqlConf.js
@@ -8,17 +8,6 @@ connexion, l'idee est fair un peu d'abstraction.
 **********************************************************/
 const mysql = require('mysql');
 const colorC = require('ansi-colors');
-let request = require('./request');
-
-/*
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "andres",
-    password: "cJpMr&(8LcR)6AvC8*",
-    database: "ademag",
-    port:3306
-});
-*/
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -27,16 +16,6 @@ const con = mysql.createConnection({
     database: "ademag",
     port:3306
 });
-
-/*
-const con = mysql.createConnection({
-    host: "remotemysql.com",
-    user: "FlufayKfhP",
-    password: "2M65cyJaUF",
-    database: "FlufayKfhP",
-    port:3306
-});
-*/
 
 con.connect((err)=>{ 
     err ? console.log(colorC.red(`problème de connection avec la base des données `+colorC.yellow('Mysql ')+ err))
