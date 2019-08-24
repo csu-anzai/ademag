3e3aa733ea59a28f2e421956682e546ae65266fa
andresvcc <andres.caballero@etu.unige.ch>
2019-08-24 08:05:54 +0200

    fix busg

--- src/client/App.js
+++ src/client/App.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
-import SignalWifiOffIcon from '@material-ui/icons/SignalWifiOff';
-import Button from '@material-ui/core/Button';
-
-import './App.css';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'. Built with '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI.
-      </Link>
-    </Typography>
-  );
-}
+import './app.css';
+import ReactImage from './react.png';
 
 const login = (data) =>{
   axios.post(`http://localhost:5002/redacteur/login`, {name:'super'}, { withCredentials: true })
@@ -84,55 +58,22 @@ const eliminate = (data) =>{
   })
 }
 
-export default function App() {
-  return (
-    <Box  >
-      <Box css={{ bgcolor: 'palevioletred', p: 1, textTransform: 'uppercase' }}>
-        <div className="App">
-          <div className="App-header">
-            <h1>id:{'aqui'}</h1>
-            <button onClick={login}>login</button>
-            <button onClick={post}>post</button>
-            <button onClick={get}>get</button>
-            <button onClick={put}>put</button>
-            <button onClick={eliminate}>delete</button>
-          </div>
-        </div>
-      </Box >
+export default class App extends Component {
+  state = { username: null };
 
-      <Box clone pt={2} pr={1} pb={1} pl={2}>
-      <Paper elevation={0}>
-        
-        <Grid container spacing={2} justify="flex-end" alignItems="center" wrap="nowrap">
-          <Grid item>
-            <Box bgcolor="primary.main" clone>
-              <Avatar>
-                <SignalWifiOffIcon />
-              </Avatar>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Typography>You have lost connection to the internet. This app is offline.</Typography>
-          </Grid>
-        </Grid>
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
 
-        <Grid container justify="flex-end" spacing={3}>
-          <Grid item>
-            <Button color="primary">Turn on wifi</Button>
-          </Grid>
-        </Grid>
-
-      </Paper>
-    </Box>
-      <Container maxWidth="sm">
-        <Box my={4}>
-            <Typography variant="h4" component="h1" gutterBottom>
-              Create React App v4-beta example
-            </Typography>
-            <ProTip />
-            <Copyright />
-          </Box>
-      </Container>
-    </Box >
-  );
+  render() {
+    const { username } = this.state;
+    return (
+      <div>
+        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <img src={ReactImage} alt="react" />
+      </div>
+    );
+  }
 }
