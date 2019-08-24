18fa6660bbd88dde7a9e898eb3db28a55e50a567
andresvcc <andres.caballero@etu.unige.ch>
2019-08-24 08:23:41 +0200

    fix bugs react

--- src/client/App.js
+++ src/client/App.js
@@ -1,79 +1,15 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
-const login = (data) =>{
-  axios.post(`http://localhost:5002/redacteur/login`, {name:'super'}, { withCredentials: true })
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => { // then print response status
-      console.log(err)
-  })
-}
 
-const post = (data) =>{
-  axios.post(`http://localhost:5002/redacteur`, {}, { withCredentials: true })
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => { // then print response status
-      console.log(err)
-  })
-}
 
-const get = (data) =>{
-  axios.get(`http://localhost:5002/redacteur/`, { withCredentials: true })
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => { // then print response status
-      console.log(err)
-  })
-}
 
-const data2 = {
-	username:"username",
-	password:"password"
-}
-
-const put = (data) =>{
-  axios.put(`http://localhost:5002/redacteur/login`, data2, { withCredentials: true })
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => { // then print response status
-      console.log(err)
-  })
-}
-
-const eliminate = (data) =>{
-  axios.delete(`http://localhost:5002/redacteur`, { withCredentials: true })
-  .then(res => {
-    console.log(res.data)
-  })
-  .catch(err => { // then print response status
-      console.log(err)
-  })
-}
-
-export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
-  render() {
-    const { username } = this.state;
+export default function App(){
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <h1>Loading.. please wait!</h1>
         <img src={ReactImage} alt="react" />
       </div>
     );
-  }
 }
