38c8544a09dc1ecc9d2166cdb932486f1f9c6c37
andresvcc <andres.caballero@etu.unige.ch>
2019-08-25 09:13:15 +0200

    add muicss

--- src/client/component/app/App.js
+++ src/client/component/app/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect }  from 'react';
-import {Button, Typography, Box, Avatar} from '@material-ui/core/';
-import ThemeProvider from '@material-ui/styles/ThemeProvider';
-import {useStyles, theme} from './styleTheme'
+import Appbar from 'muicss/lib/react/appbar';
+import Button from 'muicss/lib/react/button';
+import Container from 'muicss/lib/react/container';
 import './App.css'
 
 import {
@@ -26,28 +26,21 @@ const createUser =()=>{
 }
 
 export default function App(){
-  
-  const classes = useStyles();
+
   useEffect(() => {
     status()
   }, []);
 
     return (
-      <Box className={classes.header}>
-        <ThemeProvider theme={theme}>
-          <Typography variant="h1">Title Exemple h1</Typography>
-          <Typography variant="h2">Title Exemple h2</Typography>
-          <Typography variant="h3">Title Exemple h3</Typography>
-          <Avatar alt="react" src={`${__API__}/images/react.png`} className={classes.bigAvatar} />
-          <Typography variant="h4">Title Exemple h4</Typography>
-          <Typography variant="h5">Title Exemple h5</Typography>
-          <Typography variant="h6">Title Exemple h6</Typography>
-          <Button variant="contained" className={classes.status} onClick={status}>status</Button>
-          <Button variant="contained" className={classes.login} onClick={loginUser}>login</Button>
-          <Button variant="contained" className={classes.logout} onClick={logout}>logout</Button>
-          <Button variant="contained" color='primary' className={classes.button} onClick={createUser}>create user</Button>
-          <Button variant="contained" color='secondary' className={classes.button} onClick={eliminate}>delete count</Button>
-        </ThemeProvider>
-      </Box>
+      <div>
+      <Appbar></Appbar>
+      <Container>
+        <Button color="primary" onClick={status}>status</Button>
+        <Button color="primary" onClick={loginUser}>loginUser</Button>
+        <Button color="primary" onClick={logout}>logout</Button>
+        <Button color="primary" onClick={createUser}>createUser</Button>
+        <Button color="primary" onClick={eliminate}>eliminate</Button>
+      </Container>
+    </div>
     );
 }
--- src/client/index.js
+++ src/client/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/app/App';
-import theme from './theme';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
-
-let themeResposive = createMuiTheme();
-themeResposive = responsiveFontSizes(themeResposive);
-
-
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <ThemeProvider theme={themeResposive}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
       <App />
-    </ThemeProvider>
-  </ThemeProvider>,
-  document.querySelector('#root')
+  ,document.getElementById('root')
 );
