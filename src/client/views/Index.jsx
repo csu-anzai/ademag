fc10c9540acd996734fa67ab9dead8b60256cbe7
andresvcc <andres.caballero@etu.unige.ch>
2019-09-02 16:21:11 +0200

    d

--- src/client/views/Index.jsx
+++ src/client/views/Index.jsx
@@ -13,7 +13,6 @@ import {
   Segment,
   Card,
   Icon
-
 } from "semantic-ui-react";
 
 /* eslint-disable react/no-multi-comp */
@@ -242,17 +241,29 @@ class Index extends Component {
             </Segment>
           </Grid.Column>
         </Grid>
+
+
         <Card>
-          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' style={{height:'500px', width:'350px'}} wrapped ui={false} />
           <Card.Content>
-            <Card.Header>Matthew</Card.Header>
+            <Card.Header >Matthew</Card.Header>
+            <Card.Header>Marie 2</Card.Header>
+            <Card.Header>pas des enfant</Card.Header>
             <Card.Meta>
               <span className='date'>Joined in 2015</span>
             </Card.Meta>
             <Card.Description>
               Matthew is a musician living in Nashville.
             </Card.Description>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
           </Card.Content>
+
+
+
+
+
           <Card.Content extra>
                   <a>
                     <Icon name='user' />
