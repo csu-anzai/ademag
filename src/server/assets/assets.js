934f99f9cdf86a9fd9f33f634459ad4531be5c36
andresvcc <andres.caballero@etu.unige.ch>
2019-09-03 11:10:44 +0200

    clean all

--- src/server/assets/assets.js
+++
@@ -1,5 +0,0 @@
-const {} = require('./mongoDB/mongoConf')
-const {} = require('./mysql/mysqlConf')
-const {} = require('./mysql/request')
-const {} = require('./utilmy/utilmy')
-const {} = require('./session/sessionConf')
